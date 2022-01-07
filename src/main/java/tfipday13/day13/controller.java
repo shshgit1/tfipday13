6fb5b95baf451bacb552df677fb273a495fdf009
shawn <shawntan2@gmail.com>
2022-01-07 16:18:46 +0800

    a

--- src/main/java/tfipday13/day13/controller.java
+++ src/main/java/tfipday13/day13/controller.java
@@ -3,19 +3,28 @@ package tfipday13.day13;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Controller
 public class controller {
+    public Logger logger=LoggerFactory.getLogger(controller.class);
     @GetMapping("/gotoform")
     public String GetToTheForm(Model modelObject){
         users NewUser=new users();
         modelObject.addAttribute("nameofuser", NewUser);
+        logger.info("going to form page");
         return "form";
     }
     @PostMapping("/contacts")
-    {
-
+    public String afterSubmission(@ModelAttribute users NewUser, Model modelObjectPostMapping)
+    { 
+        logger.info("from form :" + NewUser.getName());
+        logger.info("password :" + NewUser.getemailAdd());
+        logger.info("number :"+NewUser.getphoneNum());
+        return "aftersubmission";
     }
     
 }
--- src/main/java/tfipday13/day13/users.java
+++ src/main/java/tfipday13/day13/users.java
@@ -2,6 +2,8 @@ package tfipday13.day13;
 
 import java.io.Serializable;
 
+import com.fasterxml.jackson.core.sym.Name;
+
 public class users implements Serializable{
     public String Name;
     public String phoneNum;
@@ -10,10 +12,20 @@ public String emailAdd;
 public String getName(){
     return this.Name;
 }
+
 public String getphoneNum(){
     return this.phoneNum;
 }
 public String getemailAdd(){
-return this.emailAdd;
+    return this.emailAdd;
+}
+public void setName(String name){
+    name=this.Name;
+}
+public void setphoneNum(String number){
+    this.phoneNum=number;
+}
+public void setemailAdd(String email){
+    this.emailAdd=email;
 }
 }
